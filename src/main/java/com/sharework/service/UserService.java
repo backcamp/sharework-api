a655e909802e1ffa806d986952c68cd6a5448321
xoghks1397 <62299120+xoghks1397@users.noreply.github.com>
2023-09-13 12:09:41 +0900

    Update src/main/java/com/sharework/service/UserService.java
    Co-authored-by: Jeesang Kim <jeenowden@gmail.com>

--- src/main/java/com/sharework/service/UserService.java
+++ src/main/java/com/sharework/service/UserService.java
@@ -39,7 +39,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.transaction.Transactional;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
