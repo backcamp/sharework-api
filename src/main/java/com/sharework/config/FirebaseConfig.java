e8184e25cc34f0cf65d0b569d5a5b4e4fc68aa8d
Jeesang Kim <jeenowden@gmail.com>
2023-10-30 21:22:46 +0900

    Add alarm simple feature (#34)
    * add alarm simple feature
    
    * update sharework-profiles

---
+++ src/main/java/com/sharework/config/FirebaseConfig.java
@@ -0,0 +1,39 @@
+package com.sharework.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.service-account.path}")
+    private String path;
+
+    @Bean
+    public void initializeFirebase() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream(path);
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+            FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
+
+            log.info("initializeFirebase done: " + firebaseApp.getName() + " With serviceAccount json: " + path);
+        } catch (FileNotFoundException e) {
+            log.error("initializeFirebase FileNotFoundException: " + e);
+        } catch (IOException e) {
+            log.error("initializeFirebase IOException: " + e);
+        }
+    }
+}
---
+++ src/main/java/com/sharework/request/model/AlarmRequest.java
@@ -0,0 +1,19 @@
+package com.sharework.request.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AlarmRequest {
+
+    @ApiModelProperty(value = "알람 대상 FCM 토큰")
+    private String targetFCMToken;
+
+    @ApiModelProperty(value = "알람 제목", example = "구직신청")
+    private String title;
+
+    @ApiModelProperty(value = "알람 본문", example = "'[함정훈]' 님으로부터 구직 신청이 들어왔습니다.")
+    private String body;
+}
