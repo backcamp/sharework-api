a88286dcb23c9f0c24047362ef74734fad405d56
xoghks1397 <62299120+xoghks1397@users.noreply.github.com>
2024-01-10 21:54:55 +0900

    Delete basic error (#63)
    * delete ImsiController
    
    * delete basicErrorController
    
    * Update src/main/java/com/sharework/config/SwaggerConfig.java
    
    Co-authored-by: Jeesang Kim <jeenowden@gmail.com>
    
    ---------
    
    Co-authored-by: Jeesang Kim <jeenowden@gmail.com>

--- src/main/java/com/sharework/config/SwaggerConfig.java
+++ src/main/java/com/sharework/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().apiInfo(apiInfo());
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select().apis(RequestHandlerSelectors.basePackage( "com.sharework.controller" )).paths(PathSelectors.any()).build().apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
